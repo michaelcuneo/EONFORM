d66bbc78831e897e9f5ae4f88cdc7cc84c08970a
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-18 13:04:31 +1000

    Add hydraulic erosion settings

--- Source/CodenameGaea/TerrainErosion.h
+++ Source/CodenameGaea/TerrainErosion.h
@@ -10,6 +10,18 @@ struct FTerrainThermalErosionSettings
 	float Strength = 0.35f;
 };
 
+struct FTerrainHydraulicErosionSettings
+{
+	int32 Iterations = 24;
+	float Rainfall = 0.01f;
+	float FlowRate = 0.55f;
+	float SedimentCapacity = 0.7f;
+	float ErosionRate = 0.18f;
+	float DepositionRate = 0.12f;
+	float Evaporation = 0.08f;
+	float MinimumSlope = 0.01f;
+};
+
 class FTerrainErosion
 {
 public:
@@ -17,4 +29,10 @@ public:
 		FTerrainHeightField& HeightField,
 		float HeightScale,
 		const FTerrainThermalErosionSettings& Settings);
+
+	static void ApplyHydraulic(
+		FTerrainHeightField& HeightField,
+		float HeightScale,
+		const FTerrainHydraulicErosionSettings& Settings,
+		TArray<float>* OutFlowAccumulation = nullptr);
 };
