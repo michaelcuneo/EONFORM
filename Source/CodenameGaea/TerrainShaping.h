c64f919810a65f6f97133ac907d0799399b69f0c
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-18 12:47:57 +1000

    Expand terrain shaping masks

--- Source/CodenameGaea/TerrainShaping.h
+++ Source/CodenameGaea/TerrainShaping.h
@@ -6,5 +6,7 @@ class FTerrainShaping
 {
 public:
 	static float BuildMountainMask(float MacroNoise, float Threshold, float TransitionWidth);
+	static float BuildFoothillMask(float MountainMask, float Width);
+	static float BuildValleyMask(float ValleyNoise, float Width, float Sharpness);
 	static float ApplySignedPower(float Value, float Exponent);
 };
