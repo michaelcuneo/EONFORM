12b5bf219205a35b7db9d3d88627b27e38615d00
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-19 22:31:45 +1000

    Add runtime terrain dynamic mesh actor

---
+++ Plugins/CodenameGaea/Source/CodenameGaeaRuntime/Public/GaeaTerrainDynamicMeshActor.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GaeaTerrainDataset.h"
+#include "GaeaTerrainDynamicMeshActor.generated.h"
+
+class UDynamicMeshComponent;
+
+/** Runtime-safe scene actor that renders a Codename Gaea terrain dataset as a Dynamic Mesh. */
+UCLASS()
+class CODENAMEGAEARUNTIME_API AGaeaTerrainDynamicMeshActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AGaeaTerrainDynamicMeshActor();
+
+	bool ApplyTerrainDataset(
+		const FGaeaTerrainDataset& Dataset,
+		float HeightScale,
+		FString* OutError = nullptr);
+
+	UDynamicMeshComponent* GetTerrainMeshComponent() const
+	{
+		return TerrainMesh;
+	}
+
+private:
+	UPROPERTY(VisibleAnywhere, Category="Codename Gaea")
+	TObjectPtr<UDynamicMeshComponent> TerrainMesh;
+};
