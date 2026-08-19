23da729b10e0a384175ed637152742e6c821de16
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-19 16:49:12 +1000

    Add Codename Gaea editor module

---
+++ Plugins/CodenameGaea/Source/CodenameGaeaEditor/CodenameGaeaEditor.Build.cs
@@ -0,0 +1,23 @@
+using UnrealBuildTool;
+
+public class CodenameGaeaEditor : ModuleRules
+{
+    public CodenameGaeaEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "CodenameGaeaCore",
+            "CodenameGaeaRuntime"
+        });
+
+        PrivateDependencyModuleNames.AddRange(new string[]
+        {
+            "UnrealEd"
+        });
+    }
+}
