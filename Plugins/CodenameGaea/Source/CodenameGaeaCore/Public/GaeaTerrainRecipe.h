5d3bd0438f43646a2d6dc4248fdda98278951b97
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-19 22:58:54 +1000

    Add terrain context node type

--- Plugins/CodenameGaea/Source/CodenameGaeaCore/Public/GaeaTerrainRecipe.h
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Public/GaeaTerrainRecipe.h
@@ -7,6 +7,7 @@ namespace GaeaTerrainNodeTypes
 {
 	CODENAMEGAEACORE_API extern const FName SourceDataset;
 	CODENAMEGAEACORE_API extern const FName ProceduralTerrain;
+	CODENAMEGAEACORE_API extern const FName TerrainContext;
 	CODENAMEGAEACORE_API extern const FName HydraulicErosion;
 }
 
