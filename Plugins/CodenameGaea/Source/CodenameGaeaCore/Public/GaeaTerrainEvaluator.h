743cc10c48d06b745ff7418cc9cf92507c70ed7d
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-19 18:23:37 +1000

    Add runtime terrain graph evaluator API

---
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Public/GaeaTerrainEvaluator.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GaeaTerrainDataset.h"
+#include "GaeaTerrainRecipe.h"
+
+struct CODENAMEGAEACORE_API FGaeaTerrainEvaluationContext
+{
+	FGaeaTerrainDataset SourceDataset;
+	float HeightScale = 1000.0f;
+};
+
+struct CODENAMEGAEACORE_API FGaeaTerrainEvaluationResult
+{
+	bool bSuccess = false;
+	FString Error;
+	uint32 RecipeHash = 0;
+	FGaeaTerrainDataset Dataset;
+};
+
+struct CODENAMEGAEACORE_API FGaeaTerrainNodeEvaluation
+{
+	FGaeaTerrainDataset Dataset;
+};
+
+using FGaeaTerrainNodeEvaluator = TFunction<bool(
+	const FGaeaTerrainNode&,
+	const TMap<FName, const FGaeaTerrainNodeEvaluation*>&,
+	const FGaeaTerrainEvaluationContext&,
+	FGaeaTerrainNodeEvaluation&,
+	FString&)>;
+
+/** Runtime registry for pure graph node evaluators. */
+class CODENAMEGAEACORE_API FGaeaTerrainNodeRegistry
+{
+public:
+	static void Register(FName NodeType, FGaeaTerrainNodeEvaluator Evaluator);
+	static bool IsRegistered(FName NodeType);
+	static void RegisterBuiltIns();
+	static void Reset();
+
+private:
+	friend class FGaeaTerrainEvaluator;
+	static const FGaeaTerrainNodeEvaluator* Find(FName NodeType);
+};
+
+/** Evaluates FGaeaTerrainRecipe without any editor-only dependency. */
+class CODENAMEGAEACORE_API FGaeaTerrainEvaluator
+{
+public:
+	static FGaeaTerrainEvaluationResult Evaluate(
+		const FGaeaTerrainRecipe& Recipe,
+		const FGaeaTerrainEvaluationContext& Context);
+};
