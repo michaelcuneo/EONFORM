f034271803da3d5b67fd9bfef223cc835471bd00
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-20 03:39:54 +1000

    label hydraulic and thermal erosion nodes explicitly

--- Plugins/CodenameGaea/Source/CodenameGaeaCore/Private/CodenameGaeaCoreModule.cpp
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Private/CodenameGaeaCoreModule.cpp
@@ -1,5 +1,7 @@
 #include "Modules/ModuleManager.h"
 
+#include "GaeaTerrainNodeDescriptor.h"
+#include "GaeaTerrainRecipe.h"
 #include "GaeaThermalErosionNode.h"
 
 class FCodenameGaeaCoreModule final : public IModuleInterface
@@ -7,6 +9,15 @@ class FCodenameGaeaCoreModule final : public IModuleInterface
 public:
 	virtual void StartupModule() override
 	{
+		FGaeaTerrainNodeDescriptorRegistry::RegisterBuiltIns();
+
+		FGaeaTerrainNodeDescriptor HydraulicDescriptor;
+		if (FGaeaTerrainNodeDescriptorRegistry::Get(GaeaTerrainNodeTypes::HydraulicErosion, HydraulicDescriptor))
+		{
+			HydraulicDescriptor.DisplayName = TEXT("Hydraulic Erosion");
+			FGaeaTerrainNodeDescriptorRegistry::Register(HydraulicDescriptor);
+		}
+
 		RegisterGaeaThermalErosionNode();
 	}
 };
