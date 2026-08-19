b1d6c7d0dde5ecb5a42c2bf1e8e2f44ef661a45a
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-19 22:47:39 +1000

    Make external source an optional graph node

--- Plugins/CodenameGaea/Source/CodenameGaeaEditor/Private/GaeaEditorGraph.cpp
+++ Plugins/CodenameGaea/Source/CodenameGaeaEditor/Private/GaeaEditorGraph.cpp
@@ -244,11 +244,6 @@ void UGaeaEditorGraphSchema::GetGraphContextActions(FGraphContextMenuBuilder& Co
 
 	for (const FGaeaTerrainNodeDescriptor& Descriptor : Descriptors)
 	{
-		if (Descriptor.Type == GaeaTerrainNodeTypes::SourceDataset)
-		{
-			continue;
-		}
-
 		ContextMenuBuilder.AddAction(MakeShared<FGaeaGraphSchemaAction_NewNode>(
 			FText::FromString(Descriptor.Category),
 			FText::FromString(Descriptor.DisplayName),
