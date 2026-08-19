37030c47dd8a7a7d07370b27c4c0b96700fae87d
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-19 17:20:09 +1000

    Add canonical terrain field names

---
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Public/GaeaTerrainFieldNames.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+namespace GaeaTerrainFieldNames
+{
+	CODENAMEGAEACORE_API extern const FName Height;
+	CODENAMEGAEACORE_API extern const FName Elevation;
+	CODENAMEGAEACORE_API extern const FName SlopeDegrees;
+	CODENAMEGAEACORE_API extern const FName Concavity;
+	CODENAMEGAEACORE_API extern const FName Convexity;
+	CODENAMEGAEACORE_API extern const FName Mountain;
+	CODENAMEGAEACORE_API extern const FName Foothill;
+	CODENAMEGAEACORE_API extern const FName Plains;
+	CODENAMEGAEACORE_API extern const FName Thermal;
+	CODENAMEGAEACORE_API extern const FName Rainfall;
+	CODENAMEGAEACORE_API extern const FName HydraulicErosion;
+	CODENAMEGAEACORE_API extern const FName Deposition;
+	CODENAMEGAEACORE_API extern const FName Evaporation;
+	CODENAMEGAEACORE_API extern const FName RockHardness;
+	CODENAMEGAEACORE_API extern const FName Weathering;
+	CODENAMEGAEACORE_API extern const FName SoilDepth;
+}
