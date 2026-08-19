7d4d8c9b392ee3f8bde3f5b2e43657ec386f3aa2
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-19 21:51:18 +1000

    Add explicit terrain graph node widget

---
+++ Plugins/CodenameGaea/Source/CodenameGaeaEditor/Private/GaeaTerrainGraphNode.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NodeFactory.h"
+#include "SGraphNode.h"
+
+class SGraphPin;
+class UEdGraphNode;
+class UEdGraphPin;
+
+/**
+ * Explicit visual node for Codename Gaea terrain nodes.
+ *
+ * Using a dedicated SGraphNode keeps input/output pin placement and pin drag
+ * behavior under our control instead of relying on GraphEditor's fallback
+ * widget for an otherwise unknown UEdGraphNode subclass.
+ */
+class SGaeaTerrainGraphNode : public SGraphNode
+{
+public:
+	SLATE_BEGIN_ARGS(SGaeaTerrainGraphNode) {}
+	SLATE_END_ARGS()
+
+	void Construct(const FArguments& InArgs, UEdGraphNode* InNode);
+
+	virtual void UpdateGraphNode() override;
+	virtual TSharedPtr<SGraphPin> CreatePinWidget(UEdGraphPin* Pin) const override;
+};
+
+class FGaeaTerrainGraphNodeFactory final : public FGraphNodeFactory
+{
+public:
+	virtual TSharedPtr<SGraphNode> CreateNodeWidget(UEdGraphNode* InNode) override;
+};
