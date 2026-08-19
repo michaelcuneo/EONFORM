3e5cc3a1c39d790ecf97b794659a9f1c17893739
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-19 17:13:01 +1000

    Back terrain context maps with Gaea fields

--- Source/CodenameGaea/TerrainContext.h
+++ Source/CodenameGaea/TerrainContext.h
@@ -5,25 +5,77 @@
 
 struct FTerrainContextMaps
 {
-	TArray<float> Elevation;
-	TArray<float> SlopeDegrees;
-	TArray<float> Concavity;
-	TArray<float> Convexity;
-	TArray<float> Mountain;
-	TArray<float> Foothill;
-	TArray<float> Plains;
+	FGaeaScalarField ElevationField;
+	FGaeaScalarField SlopeDegreesField;
+	FGaeaScalarField ConcavityField;
+	FGaeaScalarField ConvexityField;
+	FGaeaScalarField MountainField;
+	FGaeaScalarField FoothillField;
+	FGaeaScalarField PlainsField;
+
+	TArray<float>& Elevation;
+	TArray<float>& SlopeDegrees;
+	TArray<float>& Concavity;
+	TArray<float>& Convexity;
+	TArray<float>& Mountain;
+	TArray<float>& Foothill;
+	TArray<float>& Plains;
+
+	FTerrainContextMaps()
+		: Elevation(ElevationField.Values)
+		, SlopeDegrees(SlopeDegreesField.Values)
+		, Concavity(ConcavityField.Values)
+		, Convexity(ConvexityField.Values)
+		, Mountain(MountainField.Values)
+		, Foothill(FoothillField.Values)
+		, Plains(PlainsField.Values)
+	{
+	}
+
+	FTerrainContextMaps(const FTerrainContextMaps& Other)
+		: ElevationField(Other.ElevationField)
+		, SlopeDegreesField(Other.SlopeDegreesField)
+		, ConcavityField(Other.ConcavityField)
+		, ConvexityField(Other.ConvexityField)
+		, MountainField(Other.MountainField)
+		, FoothillField(Other.FoothillField)
+		, PlainsField(Other.PlainsField)
+		, Elevation(ElevationField.Values)
+		, SlopeDegrees(SlopeDegreesField.Values)
+		, Concavity(ConcavityField.Values)
+		, Convexity(ConvexityField.Values)
+		, Mountain(MountainField.Values)
+		, Foothill(FoothillField.Values)
+		, Plains(PlainsField.Values)
+	{
+	}
+
+	FTerrainContextMaps& operator=(const FTerrainContextMaps& Other)
+	{
+		if (this != &Other)
+		{
+			ElevationField = Other.ElevationField;
+			SlopeDegreesField = Other.SlopeDegreesField;
+			ConcavityField = Other.ConcavityField;
+			ConvexityField = Other.ConvexityField;
+			MountainField = Other.MountainField;
+			FoothillField = Other.FoothillField;
+			PlainsField = Other.PlainsField;
+		}
+		return *this;
+	}
 
 	bool IsValidFor(const FTerrainHeightField& HeightField) const
 	{
-		const int32 NumCells = HeightField.Data.Num();
+		const FGaeaGridDomain& Domain = HeightField.GetGaeaDomain();
 		return HeightField.IsValid()
-			&& Elevation.Num() == NumCells
-			&& SlopeDegrees.Num() == NumCells
-			&& Concavity.Num() == NumCells
-			&& Convexity.Num() == NumCells
-			&& Mountain.Num() == NumCells
-			&& Foothill.Num() == NumCells
-			&& Plains.Num() == NumCells;
+			&& ElevationField.IsValid() && ElevationField.Domain == Domain
+			&& SlopeDegreesField.IsValid() && SlopeDegreesField.Domain == Domain
+			&& ConcavityField.IsValid() && ConcavityField.Domain == Domain
+			&& ConvexityField.IsValid() && ConvexityField.Domain == Domain
+			&& MountainField.IsValid() && MountainField.Domain == Domain
+			&& FoothillField.IsValid() && FoothillField.Domain == Domain
+			&& PlainsField.IsValid() && PlainsField.Domain == Domain;
 	}
 };
 
@@ -37,21 +89,63 @@ struct FTerrainProcessMaskSettings
 
 struct FTerrainProcessMasks
 {
-	TArray<float> Thermal;
-	TArray<float> Rainfall;
-	TArray<float> HydraulicErosion;
-	TArray<float> Deposition;
-	TArray<float> Evaporation;
+	FGaeaScalarField ThermalField;
+	FGaeaScalarField RainfallField;
+	FGaeaScalarField HydraulicErosionField;
+	FGaeaScalarField DepositionField;
+	FGaeaScalarField EvaporationField;
+
+	TArray<float>& Thermal;
+	TArray<float>& Rainfall;
+	TArray<float>& HydraulicErosion;
+	TArray<float>& Deposition;
+	TArray<float>& Evaporation;
+
+	FTerrainProcessMasks()
+		: Thermal(ThermalField.Values)
+		, Rainfall(RainfallField.Values)
+		, HydraulicErosion(HydraulicErosionField.Values)
+		, Deposition(DepositionField.Values)
+		, Evaporation(EvaporationField.Values)
+	{
+	}
+
+	FTerrainProcessMasks(const FTerrainProcessMasks& Other)
+		: ThermalField(Other.ThermalField)
+		, RainfallField(Other.RainfallField)
+		, HydraulicErosionField(Other.HydraulicErosionField)
+		, DepositionField(Other.DepositionField)
+		, EvaporationField(Other.EvaporationField)
+		, Thermal(ThermalField.Values)
+		, Rainfall(RainfallField.Values)
+		, HydraulicErosion(HydraulicErosionField.Values)
+		, Deposition(DepositionField.Values)
+		, Evaporation(EvaporationField.Values)
+	{
+	}
+
+	FTerrainProcessMasks& operator=(const FTerrainProcessMasks& Other)
+	{
+		if (this != &Other)
+		{
+			ThermalField = Other.ThermalField;
+			RainfallField = Other.RainfallField;
+			HydraulicErosionField = Other.HydraulicErosionField;
+			DepositionField = Other.DepositionField;
+			EvaporationField = Other.EvaporationField;
+		}
+		return *this;
+	}
 
 	bool IsValidFor(const FTerrainHeightField& HeightField) const
 	{
-		const int32 NumCells = HeightField.Data.Num();
+		const FGaeaGridDomain& Domain = HeightField.GetGaeaDomain();
 		return HeightField.IsValid()
-			&& Thermal.Num() == NumCells
-			&& Rainfall.Num() == NumCells
-			&& HydraulicErosion.Num() == NumCells
-			&& Deposition.Num() == NumCells
-			&& Evaporation.Num() == NumCells;
+			&& ThermalField.IsValid() && ThermalField.Domain == Domain
+			&& RainfallField.IsValid() && RainfallField.Domain == Domain
+			&& HydraulicErosionField.IsValid() && HydraulicErosionField.Domain == Domain
+			&& DepositionField.IsValid() && DepositionField.Domain == Domain
+			&& EvaporationField.IsValid() && EvaporationField.Domain == Domain;
 	}
 };
 
