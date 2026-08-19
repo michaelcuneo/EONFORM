b12c0a69d3f8452c71a8cf3fdf514bcd44643857
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-19 19:41:47 +1000

    Register terrain graph pin factory

--- Plugins/CodenameGaea/Source/CodenameGaeaEditor/Private/CodenameGaeaEditorModule.cpp
+++ Plugins/CodenameGaea/Source/CodenameGaeaEditor/Private/CodenameGaeaEditorModule.cpp
@@ -1,8 +1,10 @@
 #include "Modules/ModuleManager.h"
 
+#include "EdGraphUtilities.h"
 #include "Framework/Application/SlateApplication.h"
 #include "Framework/Commands/UIAction.h"
 #include "Framework/Docking/TabManager.h"
+#include "GaeaTerrainGraphPin.h"
 #include "SGaeaTerrainInspector.h"
 #include "ToolMenus.h"
 #include "Widgets/Docking/SDockTab.h"
@@ -19,6 +21,9 @@ class FCodenameGaeaEditorModule : public IModuleInterface
 public:
 	virtual void StartupModule() override
 	{
+		TerrainGraphPinFactory = MakeShared<FGaeaTerrainGraphPinFactory>();
+		FEdGraphUtilities::RegisterVisualPinFactory(TerrainGraphPinFactory);
+
 		FGlobalTabmanager::Get()->RegisterNomadTabSpawner(
 			CodenameGaeaTabName,
 			FOnSpawnTab::CreateRaw(this, &FCodenameGaeaEditorModule::SpawnCodenameGaeaTab))
@@ -32,6 +37,12 @@ public:
 
 	virtual void ShutdownModule() override
 	{
+		if (TerrainGraphPinFactory.IsValid())
+		{
+			FEdGraphUtilities::UnregisterVisualPinFactory(TerrainGraphPinFactory);
+			TerrainGraphPinFactory.Reset();
+		}
+
 		UToolMenus::UnRegisterStartupCallback(this);
 		UToolMenus::UnregisterOwner(this);
 
@@ -68,6 +79,8 @@ private:
 				SNew(SGaeaTerrainInspector)
 			];
 	}
+
+	TSharedPtr<FGaeaTerrainGraphPinFactory> TerrainGraphPinFactory;
 };
 
 IMPLEMENT_MODULE(FCodenameGaeaEditorModule, CodenameGaeaEditor)
