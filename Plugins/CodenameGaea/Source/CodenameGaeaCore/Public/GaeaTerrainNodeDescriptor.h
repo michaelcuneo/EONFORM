65bae4ae84dc73055c4a82830ec39ea1844a3d2b
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-19 19:08:10 +1000

    Add runtime terrain node descriptors

---
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Public/GaeaTerrainNodeDescriptor.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+enum class EGaeaTerrainParameterType : uint8
+{
+	Number,
+	Integer,
+	Boolean,
+	Name
+};
+
+struct CODENAMEGAEACORE_API FGaeaTerrainPortDescriptor
+{
+	FName Name = NAME_None;
+	FName DataType = NAME_None;
+};
+
+struct CODENAMEGAEACORE_API FGaeaTerrainParameterDescriptor
+{
+	FName Name = NAME_None;
+	FString DisplayName;
+	EGaeaTerrainParameterType Type = EGaeaTerrainParameterType::Number;
+
+	double DefaultNumber = 0.0;
+	int64 DefaultInteger = 0;
+	bool DefaultBoolean = false;
+	FName DefaultName = NAME_None;
+
+	bool bHasMinimum = false;
+	double Minimum = 0.0;
+	bool bHasMaximum = false;
+	double Maximum = 0.0;
+};
+
+struct CODENAMEGAEACORE_API FGaeaTerrainNodeDescriptor
+{
+	FName Type = NAME_None;
+	FString DisplayName;
+	FString Category;
+	FString Description;
+	TArray<FGaeaTerrainPortDescriptor> Inputs;
+	TArray<FGaeaTerrainPortDescriptor> Outputs;
+	TArray<FGaeaTerrainParameterDescriptor> Parameters;
+};
+
+/**
+ * Runtime-safe metadata describing terrain node types.
+ *
+ * The professional editor and constrained game-facing authoring layers both
+ * consume these descriptors, while execution remains owned by the runtime
+ * evaluator registry.
+ */
+class CODENAMEGAEACORE_API FGaeaTerrainNodeDescriptorRegistry
+{
+public:
+	static void Register(const FGaeaTerrainNodeDescriptor& Descriptor);
+	static void RegisterBuiltIns();
+	static bool Get(FName NodeType, FGaeaTerrainNodeDescriptor& OutDescriptor);
+	static void GetAll(TArray<FGaeaTerrainNodeDescriptor>& OutDescriptors);
+	static void Reset();
+};
