2bcf99c951472c8ec5806c76cd5af1bacd2066c2
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-22 01:43:02 +1000

    Safely snapshot active physical context without static init recursion

--- Plugins/CodenameGaea/Source/CodenameGaeaCore/Private/GaeaTerrainPhysicalMetrics.cpp
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Private/GaeaTerrainPhysicalMetrics.cpp
@@ -2,26 +2,47 @@
 
 namespace
 {
-	FCriticalSection PhysicalContextMutex;
-	FGaeaTerrainPhysicalMetrics ActivePhysicalMetrics;
-	uint64 PhysicalContextRevision = 1;
+	struct FGaeaTerrainPhysicalContextState
+	{
+		FCriticalSection Mutex;
+		FGaeaTerrainPhysicalMetrics Metrics = FGaeaTerrainPhysicalMetrics(0.0, 0.0, 0.0, 0.0);
+		uint64 Revision = 1;
+	};
+
+	FGaeaTerrainPhysicalContextState& GetPhysicalContextState()
+	{
+		static FGaeaTerrainPhysicalContextState State;
+		return State;
+	}
+}
+
+FGaeaTerrainPhysicalMetrics::FGaeaTerrainPhysicalMetrics()
+{
+	const FGaeaTerrainPhysicalMetrics Active = FGaeaTerrainPhysicalContext::GetActive();
+	WorldWidthMeters = Active.WorldWidthMeters;
+	WorldDepthMeters = Active.WorldDepthMeters;
+	ElevationScaleMeters = Active.ElevationScaleMeters;
+	SeaLevelMeters = Active.SeaLevelMeters;
 }
 
 void FGaeaTerrainPhysicalContext::SetActive(const FGaeaTerrainPhysicalMetrics& Metrics)
 {
-	FScopeLock Lock(&PhysicalContextMutex);
-	ActivePhysicalMetrics = Metrics;
-	++PhysicalContextRevision;
+	FGaeaTerrainPhysicalContextState& State = GetPhysicalContextState();
+	FScopeLock Lock(&State.Mutex);
+	State.Metrics = Metrics;
+	++State.Revision;
 }
 
 FGaeaTerrainPhysicalMetrics FGaeaTerrainPhysicalContext::GetActive()
 {
-	FScopeLock Lock(&PhysicalContextMutex);
-	return ActivePhysicalMetrics;
+	FGaeaTerrainPhysicalContextState& State = GetPhysicalContextState();
+	FScopeLock Lock(&State.Mutex);
+	return State.Metrics;
 }
 
 uint64 FGaeaTerrainPhysicalContext::GetRevision()
 {
-	FScopeLock Lock(&PhysicalContextMutex);
-	return PhysicalContextRevision;
+	FGaeaTerrainPhysicalContextState& State = GetPhysicalContextState();
+	FScopeLock Lock(&State.Mutex);
+	return State.Revision;
 }
