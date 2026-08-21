54afc081b94643149dac92d33f877f73518c5bf9
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-21 21:35:51 +1000

    Add Modify profile node declarations

---
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Private/GaeaModifyProfileNodes.h
@@ -0,0 +1,8 @@
+#pragma once
+
+void RegisterGaeaCurveNode();
+void RegisterGaeaFilterNode();
+void RegisterGaeaGraphicEQNode();
+void RegisterGaeaHealNode();
+void RegisterGaeaMatchNode();
+void RegisterGaeaPixelateNode();
