b5def14a800a5d3265641d6cbdd4352ba30e815d
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-21 21:46:42 +1000

    Add remaining Modify spatial node declarations

---
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Private/GaeaModifySpatialNodes.h
@@ -0,0 +1,14 @@
+#pragma once
+
+void RegisterGaeaDirectionalWarpNode();
+void RegisterGaeaFoldNode();
+void RegisterGaeaMeshifyNode();
+void RegisterGaeaOrigamiNode();
+void RegisterGaeaSlopeBlurNode();
+void RegisterGaeaSlopeWarpNode();
+void RegisterGaeaSwirlNode();
+void RegisterGaeaThermalShaperNode();
+void RegisterGaeaTransposeNode();
+void RegisterGaeaVariableBlurNode();
+void RegisterGaeaWarpNode();
+void RegisterGaeaWhorlNode();
