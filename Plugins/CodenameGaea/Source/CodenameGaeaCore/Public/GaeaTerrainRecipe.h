bc4dd0c5b0aa22993c3070f65cfe89273688a017
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-21 16:34:42 +1000

    add Gaea Distance node type

--- Plugins/CodenameGaea/Source/CodenameGaeaCore/Public/GaeaTerrainRecipe.h
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Public/GaeaTerrainRecipe.h
@@ -26,6 +26,7 @@ namespace GaeaTerrainNodeTypes
 	CODENAMEGAEACORE_API extern const FName Threshold;
 	CODENAMEGAEACORE_API extern const FName Transform;
 	CODENAMEGAEACORE_API extern const FName ZeroBorders;
+	CODENAMEGAEACORE_API extern const FName Distance;
 	CODENAMEGAEACORE_API extern const FName Slope;
 	CODENAMEGAEACORE_API extern const FName Angle;
 	CODENAMEGAEACORE_API extern const FName Curvature;
