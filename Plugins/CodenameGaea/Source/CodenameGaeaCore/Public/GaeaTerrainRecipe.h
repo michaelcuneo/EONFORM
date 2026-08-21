bf688353b9c4d0ea03ac38fa96e0ea3ef87e4b86
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-21 15:10:40 +1000

    add Gaea Clamp node type

--- Plugins/CodenameGaea/Source/CodenameGaeaCore/Public/GaeaTerrainRecipe.h
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Public/GaeaTerrainRecipe.h
@@ -14,6 +14,7 @@ namespace GaeaTerrainNodeTypes
 	CODENAMEGAEACORE_API extern const FName ThermalErosion;
 	CODENAMEGAEACORE_API extern const FName HydraulicErosion;
 	CODENAMEGAEACORE_API extern const FName Combine;
+	CODENAMEGAEACORE_API extern const FName Clamp;
 	CODENAMEGAEACORE_API extern const FName Slope;
 	CODENAMEGAEACORE_API extern const FName Angle;
 	CODENAMEGAEACORE_API extern const FName Curvature;
