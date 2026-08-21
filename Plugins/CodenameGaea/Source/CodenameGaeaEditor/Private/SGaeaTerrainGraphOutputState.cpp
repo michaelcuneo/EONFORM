379d0ab47a6454897d2b387d464443bfb3602d59
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-22 00:41:37 +1000

    Synchronize terrain output state with active graph asset

---
+++ Plugins/CodenameGaea/Source/CodenameGaeaEditor/Private/SGaeaTerrainGraphOutputState.cpp
@@ -0,0 +1,40 @@
+#include "SGaeaTerrainGraphPanel.h"
+
+#include "GaeaTerrainOutputEditorState.h"
+
+void SGaeaTerrainGraphPanel::SyncOutputSettingsState()
+{
+	FGaeaTerrainOutputEditorState& OutputState = FGaeaTerrainOutputEditorState::Get();
+	UGaeaTerrainGraphAsset* Asset = CurrentAsset.Get();
+
+	if (Asset != LastOutputSettingsAsset.Get())
+	{
+		LastOutputSettingsAsset = Asset;
+		if (Asset)
+		{
+			OutputState.Load(Asset->OutputSettings);
+		}
+		else
+		{
+			OutputState.Reset();
+		}
+		LastOutputSettingsRevision = OutputState.GetRevision();
+		return;
+	}
+
+	if (!Asset)
+	{
+		return;
+	}
+
+	const uint64 StateRevision = OutputState.GetRevision();
+	if (StateRevision == LastOutputSettingsRevision)
+	{
+		return;
+	}
+
+	Asset->Modify();
+	Asset->OutputSettings = OutputState.GetSettings();
+	Asset->MarkPackageDirty();
+	LastOutputSettingsRevision = StateRevision;
+}
