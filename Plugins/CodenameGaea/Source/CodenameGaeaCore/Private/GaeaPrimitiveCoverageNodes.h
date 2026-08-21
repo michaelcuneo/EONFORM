795adeb4b65a9b962555b5400566fc55e9b33567
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-21 20:07:21 +1000

    add remaining procedural Primitive declarations

---
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Private/GaeaPrimitiveCoverageNodes.h
@@ -0,0 +1,9 @@
+#pragma once
+
+void RegisterGaeaLineNoiseNode();
+void RegisterGaeaMultiFractalNode();
+void RegisterGaeaNoiseNode();
+void RegisterGaeaPatternNode();
+void RegisterGaeaShapeNode();
+void RegisterGaeaVoronoiNode();
+void RegisterGaeaWaveShineNode();
