d17d672d55237871ccdf0cf859071da47d3f9624
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-21 20:42:23 +1000

    use ImageCore for safe File node decoding

--- Plugins/CodenameGaea/Source/CodenameGaeaCore/CodenameGaeaCore.Build.cs
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/CodenameGaeaCore.Build.cs
@@ -14,6 +14,7 @@ public class CodenameGaeaCore : ModuleRules
 
         PrivateDependencyModuleNames.AddRange(new string[]
         {
+            "ImageCore",
             "ImageWrapper"
         });
     }
