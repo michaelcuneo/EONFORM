6f601bb2787f0240c71ce4d195b66042193c9aa1
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-21 16:38:37 +1000

    add Gaea FractalTerraces node type

--- Plugins/CodenameGaea/Source/CodenameGaeaCore/Public/GaeaTerrainRecipe.h
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Public/GaeaTerrainRecipe.h
@@ -27,6 +27,7 @@ namespace GaeaTerrainNodeTypes
 	CODENAMEGAEACORE_API extern const FName Transform;
 	CODENAMEGAEACORE_API extern const FName ZeroBorders;
 	CODENAMEGAEACORE_API extern const FName Distance;
+	CODENAMEGAEACORE_API extern const FName FractalTerraces;
 	CODENAMEGAEACORE_API extern const FName Slope;
 	CODENAMEGAEACORE_API extern const FName Angle;
 	CODENAMEGAEACORE_API extern const FName Curvature;
