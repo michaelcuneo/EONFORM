feb899a40bcc740f8d848ff87c180ee597c18cb1
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-21 18:09:52 +1000

    replace analysis grid state with texture preview state

--- Plugins/CodenameGaea/Source/CodenameGaeaEditor/Private/SGaeaTerrainInspector.h
+++ Plugins/CodenameGaea/Source/CodenameGaeaEditor/Private/SGaeaTerrainInspector.h
@@ -2,10 +2,13 @@
 
 #include "CoreMinimal.h"
 #include "GaeaTerrainDatasetRegistry.h"
+#include "Styling/SlateBrush.h"
+#include "UObject/StrongObjectPtr.h"
 #include "Widgets/SCompoundWidget.h"
 #include "Widgets/Views/SListView.h"
 
-class SUniformGridPanel;
+class SImage;
+class UTexture2D;
 
 class SGaeaTerrainInspector : public SCompoundWidget
 {
@@ -21,10 +24,12 @@ private:
 	TSharedRef<ITableRow> GenerateFieldRow(TSharedPtr<FName> Item, const TSharedRef<STableViewBase>& OwnerTable);
 	void OnFieldSelectionChanged(TSharedPtr<FName> Item, ESelectInfo::Type SelectInfo);
 	void RebuildPreview();
+	void ClearPreview();
 
 	const FGaeaScalarField* GetSelectedField() const;
 	FText GetSourceText() const;
 	FText GetFieldMetadataText() const;
+	FText GetPreviewStatsText() const;
 	FText GetEmptyStateText() const;
 
 	FGaeaTerrainDatasetSnapshot Snapshot;
@@ -32,5 +37,13 @@ private:
 	FName SelectedFieldName = NAME_None;
 
 	TSharedPtr<SListView<TSharedPtr<FName>>> FieldListView;
-	TSharedPtr<SUniformGridPanel> PreviewGrid;
+	TSharedPtr<SImage> PreviewImage;
+	FSlateBrush PreviewBrush;
+	TStrongObjectPtr<UTexture2D> PreviewTexture;
+
+	float PreviewMinValue = 0.0f;
+	float PreviewMaxValue = 0.0f;
+	float PreviewMeanValue = 0.0f;
+	float PreviewStdDev = 0.0f;
+	int64 PreviewSampleCount = 0;
 };
