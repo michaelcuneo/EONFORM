e291aea1196bdc61c98ab9c21ee96c50f6710c2f
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-21 16:23:36 +1000

    add Gaea Transform node type

--- Plugins/CodenameGaea/Source/CodenameGaeaCore/Public/GaeaTerrainRecipe.h
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Public/GaeaTerrainRecipe.h
@@ -24,6 +24,7 @@ namespace GaeaTerrainNodeTypes
 	CODENAMEGAEACORE_API extern const FName Sharpen;
 	CODENAMEGAEACORE_API extern const FName Sine;
 	CODENAMEGAEACORE_API extern const FName Threshold;
+	CODENAMEGAEACORE_API extern const FName Transform;
 	CODENAMEGAEACORE_API extern const FName Slope;
 	CODENAMEGAEACORE_API extern const FName Angle;
 	CODENAMEGAEACORE_API extern const FName Curvature;
