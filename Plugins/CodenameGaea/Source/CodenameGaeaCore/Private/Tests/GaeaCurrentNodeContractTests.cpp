2368454e975e2bbe6abebf15816cde6140bc90ac
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-21 19:23:19 +1000

    lock Cellular3D and Cone public contracts

--- Plugins/CodenameGaea/Source/CodenameGaeaCore/Private/Tests/GaeaCurrentNodeContractTests.cpp
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Private/Tests/GaeaCurrentNodeContractTests.cpp
@@ -36,6 +36,8 @@ bool FGaeaCurrentNodeContractTest::RunTest(const FString& Parameters)
 
 	CheckNode(*this, GaeaTerrainNodeTypes::PerlinNoise, TEXT("Perlin"), TEXT("Primitive"), 14);
 	CheckNode(*this, GaeaTerrainNodeTypes::Cellular, TEXT("Cellular"), TEXT("Primitive"), 10);
+	CheckNode(*this, GaeaTerrainNodeTypes::Cellular3D, TEXT("Cellular3D"), TEXT("Primitive"), 9);
+	CheckNode(*this, GaeaTerrainNodeTypes::Cone, TEXT("Cone"), TEXT("Primitive"), 4);
 	CheckNode(*this, GaeaTerrainNodeTypes::HydraulicErosion, TEXT("Erosion"), TEXT("Simulate"), 20);
 	CheckNode(*this, GaeaTerrainNodeTypes::ThermalErosion, TEXT("Thermal"), TEXT("Simulate"), 11);
 
