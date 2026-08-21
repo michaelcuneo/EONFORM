04c1b8eb7966badd0708fa5533e2df35e22b3e4f
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-21 21:28:21 +1000

    Add Modify foundation node declarations

---
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Private/GaeaModifyFoundationNodes.h
@@ -0,0 +1,11 @@
+#pragma once
+
+void RegisterGaeaAdjustNode();
+void RegisterGaeaApertureNode();
+void RegisterGaeaBlobRemoverNode();
+void RegisterGaeaClipNode();
+void RegisterGaeaDeflateNode();
+void RegisterGaeaDilateNode();
+void RegisterGaeaEqualizeNode();
+void RegisterGaeaExtendNode();
+void RegisterGaeaMedianNode();
