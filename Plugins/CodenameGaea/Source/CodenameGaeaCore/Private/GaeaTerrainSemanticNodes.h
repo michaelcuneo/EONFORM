83a8d5dd39667145d4dc493b3831cc3b59d7397c
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-23 15:28:33 +1000

    Expose terrain semantic derive node types

--- Plugins/CodenameGaea/Source/CodenameGaeaCore/Private/GaeaTerrainSemanticNodes.h
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Private/GaeaTerrainSemanticNodes.h
@@ -1,3 +1,12 @@
 #pragma once
 
+#include "CoreMinimal.h"
+
+namespace GaeaTerrainNodeTypes
+{
+	CODENAMEGAEACORE_API extern const FName Peaks;
+	CODENAMEGAEACORE_API extern const FName RockMap;
+	CODENAMEGAEACORE_API extern const FName Soil;
+}
+
 void RegisterGaeaTerrainSemanticNodes();
