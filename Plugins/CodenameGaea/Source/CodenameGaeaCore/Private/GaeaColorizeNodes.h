6b88f3a77430d4a0aca31a765f67ce82678e1ae6
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-23 22:22:46 +1000

    Add SatMap colorize node declaration

---
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Private/GaeaColorizeNodes.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+namespace GaeaTerrainNodeTypes
+{
+	CODENAMEGAEACORE_API extern const FName SatMap;
+}
+
+/** Registers EONFORM Colorize nodes. */
+void RegisterGaeaColorizeNodes();
