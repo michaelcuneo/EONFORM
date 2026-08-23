1737686bb3cfd2182568996a591bf2b067728703
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-23 15:28:49 +1000

    Define terrain semantic derive node types

---
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Private/GaeaTerrainSemanticNodeTypes.cpp
@@ -0,0 +1,8 @@
+#include "GaeaTerrainSemanticNodes.h"
+
+namespace GaeaTerrainNodeTypes
+{
+	const FName Peaks(TEXT("Peaks"));
+	const FName RockMap(TEXT("RockMap"));
+	const FName Soil(TEXT("Soil"));
+}
