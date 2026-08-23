391e7bf13d9d5e8d48742de2255b3745a71ed89b
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-23 15:39:19 +1000

    Use public recipe node type declarations

--- Plugins/CodenameGaea/Source/CodenameGaeaCore/Private/GaeaTerrainSemanticNodes.h
+++ Plugins/CodenameGaea/Source/CodenameGaeaCore/Private/GaeaTerrainSemanticNodes.h
@@ -1,12 +1,3 @@
 #pragma once
 
-#include "CoreMinimal.h"
-
-namespace GaeaTerrainNodeTypes
-{
-	CODENAMEGAEACORE_API extern const FName Peaks;
-	CODENAMEGAEACORE_API extern const FName RockMap;
-	CODENAMEGAEACORE_API extern const FName Soil;
-}
-
 void RegisterGaeaTerrainSemanticNodes();
