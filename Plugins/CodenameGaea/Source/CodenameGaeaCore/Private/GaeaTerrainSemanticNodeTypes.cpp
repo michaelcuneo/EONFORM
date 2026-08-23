4134333a88b79a4097ce11cd5ea37da17ea24358
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-23 15:39:14 +1000

    Remove duplicate private semantic node type definitions

--- Plugins/CodenameGaea/Source/CodenameGaeaCore/Private/GaeaTerrainSemanticNodeTypes.cpp
+++
@@ -1,8 +0,0 @@
-#include "GaeaTerrainSemanticNodes.h"
-
-namespace GaeaTerrainNodeTypes
-{
-	const FName Peaks(TEXT("Peaks"));
-	const FName RockMap(TEXT("RockMap"));
-	const FName Soil(TEXT("Soil"));
-}
