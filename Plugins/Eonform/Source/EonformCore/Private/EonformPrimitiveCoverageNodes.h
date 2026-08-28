160f260201764059b58b42f042c9452b1d649961
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-28 16:08:27 +1000

    Remove duplicate Voronoi ownership

--- Plugins/Eonform/Source/EonformCore/Private/EonformPrimitiveCoverageNodes.h
+++ Plugins/Eonform/Source/EonformCore/Private/EonformPrimitiveCoverageNodes.h
@@ -5,5 +5,4 @@ void RegisterEonformMultiFractalNode();
 void RegisterEonformNoiseNode();
 void RegisterEonformPatternNode();
 void RegisterEonformShapeNode();
-void RegisterEonformVoronoiNode();
 void RegisterEonformWaveShineNode();
