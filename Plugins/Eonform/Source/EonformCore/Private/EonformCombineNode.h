3ec97790e76bec1c7a34569dd6eeb2f78c1eb0d2
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-28 16:11:20 +1000

    Make Combine node authoritative

--- Plugins/Eonform/Source/EonformCore/Private/EonformCombineNode.h
+++ Plugins/Eonform/Source/EonformCore/Private/EonformCombineNode.h
@@ -1,3 +1,19 @@
 #pragma once
 
+#include "CoreMinimal.h"
+#include "EonformScalarField.h"
+
+namespace EonformCombine
+{
+	float ApplyMode(float A, float B, FName Mode);
+
+	bool ApplyRawFields(
+		const FEonformScalarField& A,
+		const FEonformScalarField& B,
+		FName Mode,
+		float Ratio,
+		FEonformScalarField& OutField,
+		FString* OutError = nullptr);
+}
+
 void RegisterEonformCombineNode();
