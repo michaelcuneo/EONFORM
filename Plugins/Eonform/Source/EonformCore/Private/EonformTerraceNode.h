c7b149ebd68c7a83997b17bc84f73e570016fcd7
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-28 16:10:28 +1000

    Make Terrace node authoritative

--- Plugins/Eonform/Source/EonformCore/Private/EonformTerraceNode.h
+++ Plugins/Eonform/Source/EonformCore/Private/EonformTerraceNode.h
@@ -1,3 +1,19 @@
 #pragma once
 
+#include "CoreMinimal.h"
+#include "EonformScalarField.h"
+
+namespace EonformTerrace
+{
+	bool ApplyNormalized(
+		const FEonformScalarField& Source01,
+		int32 TerraceCount,
+		float Uniformity,
+		float Steepness,
+		float Intensity,
+		int32 Seed,
+		FEonformScalarField& OutField01,
+		FString* OutError = nullptr);
+}
+
 void RegisterEonformTerraceNode();
