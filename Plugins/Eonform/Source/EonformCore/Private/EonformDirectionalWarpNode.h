79c304152a9f34682c0d701df162c018352d808a
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-29 11:07:43 +1000

    Expose authoritative DirectionalWarp point sampling

--- Plugins/Eonform/Source/EonformCore/Private/EonformDirectionalWarpNode.h
+++ Plugins/Eonform/Source/EonformCore/Private/EonformDirectionalWarpNode.h
@@ -11,6 +11,24 @@ namespace EonformDirectionalWarp
 		Mirror
 	};
 
+	/** Resolve the exact continuous source coordinate for one DirectionalWarp lattice sample. */
+	FVector2D ResolveSourceCoordinate(
+		const FVector2D& LatticeCoordinate,
+		float CustomValue,
+		float StrengthPixels,
+		float DirectionDegrees);
+
+	/**
+	 * Authoritative bilinear sampler used by DirectionalWarp. SampleInteger is
+	 * evaluated only at resolved full-world integer lattice coordinates.
+	 */
+	float SampleBilinear(
+		const TFunctionRef<float(int32, int32)>& SampleInteger,
+		const FIntPoint& Dimensions,
+		float X,
+		float Y,
+		EEdgeBehaviour EdgeBehaviour);
+
 	bool ApplyPixels(
 		const FEonformScalarField& Source,
 		const FEonformScalarField& Custom,
